a80ee1b1a05019f7aa333ea1ba7f146907c3fd93
harishpalsande9 <harishpalsande9>
2022-07-08 16:38:48 +0530

    How to replace all occurrences of a certain word in a string?

---
+++ test2.js
@@ -0,0 +1,10 @@
+console.log("How to convert 1 to true or 0 to false?");
+
+let isActive = 0;
+console.log(!!isActive);
+
+//Or
+console.log(Boolean(isActive));
+
+//Or
+console.log(isActive === 1);
