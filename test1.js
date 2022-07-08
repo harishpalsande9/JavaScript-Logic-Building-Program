369f315d88730934d8ee66db429d503e43d59c76
harishpalsande9 <harishpalsande9>
2022-07-08 16:32:33 +0530

    How to replace all occurrences of a certain word in a string?

--- test1.js
+++ test1.js
@@ -1,1 +1,14 @@
-console.log("Harish");
+console.log("How to replace all occurrences of a certain word in a string?");
+let text = "Harish&Om&Palsande";
+console.log(text);
+
+let newText = text.replace("&", " ");
+console.log(newText);
+// Only the first one replaced
+
+newText = text.replaceAll("&", " ");
+console.log(newText);
+// All Replaced
+
+newText = newText.replace("Palsande", " Chandrakant Palsande");
+console.log(newText);
