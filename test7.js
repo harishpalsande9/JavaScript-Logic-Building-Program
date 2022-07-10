590dc2ff013595c026e04071d800fcc596eba737
harishpalsande9 <harishpalsande9>
2022-07-10 13:50:31 +0530

    first to know wins. Subject: switch case statement

---
+++ test7.js
@@ -0,0 +1,15 @@
+console.log("first to know wins. Subject: switch case statement");
+
+let number = null;
+switch (number) {
+  case undefined:
+    console.log(undefined);
+    break;
+  case null:
+    console.log(null);
+    break;
+  default:
+    console.log("Non Of Them");
+}
+
+// The Output is Null
