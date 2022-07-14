a04e455db1c6f818b4865cb3586d2f86afa06a29
harishpalsande9 <harishpalsande9>
2022-07-14 11:34:33 +0530

    How do you trim whitespaces from a string in JavaScript?

---
+++ 15test.js
@@ -0,0 +1,8 @@
+console.log("How do you trim whitespaces from a string in JavaScript?");
+
+let one = "Harish";
+let two = "Om";
+
+console.log(two + one + two);
+console.log(two + one.trim() + two);
+console.log(two + one.trimEnd() + two);
