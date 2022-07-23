aeaa00ba9bba1fede3f505a2ceb5abc1fadcc6a3
harishpalsande9 <harishpalsande9>
2022-07-23 21:08:52 +0530

    JavaScript challenge; first to know wins. Subject: return statement

---
+++ 30test.js
@@ -0,0 +1,12 @@
+console.log(
+  "JavaScript challenge; first to know wins. Subject: return statement"
+);
+
+function calculate(number1, number2) {
+  return;
+  ((number1 % number2) * number1 ** 2) / 2;
+}
+
+console.log(calculate(1, 2));
+
+// Undefine
