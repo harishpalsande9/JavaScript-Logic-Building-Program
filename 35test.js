b9bccd004ea481019bb87b70ce5bd9b813709831
harishpalsande9 <harishpalsande9@gmail.com>
2022-07-27 16:48:02 +0530

    JavaScript challenge; first to know wins. Subject: Equality of objects

---
+++ 35test.js
@@ -0,0 +1,8 @@
+console.log(
+  "JavaScript challenge; first to know wins. Subject: Equality of objects"
+);
+
+const a = { name: "Harish", surname: "Palsande" };
+const b = { name: "Harish", surname: "Palsande" };
+
+console.log(a == b || a === b);
